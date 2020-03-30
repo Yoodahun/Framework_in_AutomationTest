90459835483ac1530d5a1507a55721b8b6d1d817
Yoodahun <dahun4032@gmail.com>
2020-03-31 00:38:49 +0900

    testng.xml in IntelliJ

---
+++ TestNG/src/section18/RunningTestcases.java
@@ -0,0 +1,25 @@
+package section18;
+
+
+import org.testng.annotations.Test;
+
+//Section18-135 Running testcaes in TestNG
+public class RunningTestcases {
+
+    @Test
+    public void Demo() {
+        System.out.println("hello");
+    }
+
+    @Test
+    public void Demo2nd(){
+        System.out.println("world !");
+
+    }
+    @Test
+    public void Demo3rd(){
+        System.out.println("world !");
+
+    }
+
+}
